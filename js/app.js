c5e1984e52dafc14e37251ef45d675dc070ec699
Pramesh Attwala <cheers@prameshattwala.com>
2021-05-29 17:12:27 -0400

    Add main menu and other navigation elements

--- js/app.js
+++ js/app.js
@@ -7,11 +7,20 @@
   \*****************************/
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 var gsap = __webpack_require__(/*! gsap/dist/gsap */ "./node_modules/gsap/dist/gsap.js").gsap;
 
 var ScrollTrigger = __webpack_require__(/*! gsap/dist/ScrollTrigger */ "./node_modules/gsap/dist/ScrollTrigger.js").ScrollTrigger;
 
+var ScrollToPlugin = __webpack_require__(/*! gsap/dist/ScrollToPlugin */ "./node_modules/gsap/dist/ScrollToPlugin.js").ScrollToPlugin;
+
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollToPlugin);
 var app = document.getElementById("app"); //Register a new timeline object
 
 var tl = gsap.timeline({
@@ -34,7 +43,7 @@ var d = 1; //animation duration
 
 var cards = document.querySelectorAll('.card');
 /**
- * Timeline animation
+ * Timeline animations
  */
 //Move the first two cards into the center position
 
@@ -47,6 +56,13 @@ tl.fromTo('.card:nth-child(2)', {
 });
 tl.fromTo('.card:nth-child(2) .container', {
   y: "-100%"
+}, {
+  y: 0,
+  duration: d,
+  ease: 'linear'
+}, "-=" + d);
+tl.fromTo('.card:nth-child(2) .nav-element', {
+  y: "-100vh"
 }, {
   y: 0,
   duration: d,
@@ -65,16 +81,23 @@ tl.fromTo('.card:nth-child(3) .container', {
   y: 0,
   duration: d,
   ease: 'linear'
+}, "-=" + d);
+tl.fromTo('.card:nth-child(3) .nav-element', {
+  y: "100vh"
+}, {
+  y: 0,
+  duration: d,
+  ease: 'linear'
 }, "-=" + d); //Hide the hero
 
-tl.to('.hero:nth-child(1)', {
+tl.to('#hero', {
   y: "100%",
   duration: 0,
   ease: 'linear'
 }); //Animate all the cards
 
 for (var i = 1; i < cards.length - 2; i += 2) {
-  //target                                  //from       //to           //duration      //delay
+  //target                                  	//from       //to           //duration      //delay
   tl.fromTo('.card:nth-child(' + (i + 1) + ')', {
     y: 0
   }, {
@@ -89,6 +112,13 @@ for (var i = 1; i < cards.length - 2; i += 2) {
     duration: d,
     ease: 'linear'
   }, "-=" + d);
+  tl.fromTo('.card:nth-child(' + (i + 1) + ') .nav-element', {
+    y: 0
+  }, {
+    y: "100vh",
+    duration: d,
+    ease: 'linear'
+  }, "-=" + d);
   tl.fromTo('.card:nth-child(' + (i + 3) + ')', {
     y: "100%"
   }, {
@@ -98,6 +128,13 @@ for (var i = 1; i < cards.length - 2; i += 2) {
   }, "-=" + d);
   tl.fromTo('.card:nth-child(' + (i + 3) + ') .container', {
     y: "-100%"
+  }, {
+    y: 0,
+    duration: d,
+    ease: 'linear'
+  }, "-=" + d);
+  tl.fromTo('.card:nth-child(' + (i + 3) + ') .nav-element', {
+    y: "-100vh"
   }, {
     y: 0,
     duration: d,
@@ -117,6 +154,13 @@ for (var i = 1; i < cards.length - 2; i += 2) {
     duration: d,
     ease: 'linear'
   }, "-=" + d);
+  tl.fromTo('.card:nth-child(' + (i + 2) + ') .nav-element', {
+    y: 0
+  }, {
+    y: "-100vh",
+    duration: d,
+    ease: 'linear'
+  }, "-=" + d);
   tl.fromTo('.card:nth-child(' + (i + 4) + ')', {
     y: "-100%"
   }, {
@@ -126,6 +170,13 @@ for (var i = 1; i < cards.length - 2; i += 2) {
   }, "-=" + d);
   tl.fromTo('.card:nth-child(' + (i + 4) + ') .container', {
     y: "100%"
+  }, {
+    y: 0,
+    duration: d,
+    ease: 'linear'
+  }, "-=" + d);
+  tl.fromTo('.card:nth-child(' + (i + 4) + ') .nav-element', {
+    y: "100vh"
   }, {
     y: 0,
     duration: d,
@@ -148,6 +199,13 @@ tl.fromTo('.card:nth-child(' + cards.length + ') .container', {
   duration: d,
   ease: 'linear'
 }, "-=" + d);
+tl.fromTo('.card:nth-child(' + cards.length + ') .nav-element', {
+  y: 0
+}, {
+  y: "100vh",
+  duration: d,
+  ease: 'linear'
+}, "-=" + d);
 tl.fromTo('.card:nth-child(' + (cards.length + 1) + ')', {
   y: 0
 }, {
@@ -161,16 +219,400 @@ tl.fromTo('.card:nth-child(' + (cards.length + 1) + ') .container', {
   y: "-100%",
   duration: d,
   ease: 'linear'
-}, "-=" + d); //Submit button hover event
+}, "-=" + d);
+tl.fromTo('.card:nth-child(' + (cards.length + 1) + ') .nav-element', {
+  y: 0
+}, {
+  y: "-100vh",
+  duration: d,
+  ease: 'linear'
+}, "-=" + d);
+/**
+ * Event handlers
+ */
 
-var lch = document.querySelector('.hero:last-child .heading');
-var sb = document.querySelector('.hero:last-child .submit');
-sb.addEventListener("mouseover", function () {
-  lch.classList.add('pause');
+var heading = document.querySelector('#the-end .heading');
+var submitButton = document.querySelector('#the-end .submit');
+var links = document.querySelectorAll('.link-home');
+var burgers = document.querySelectorAll('.hamburger-menu');
+var subMenu = document.querySelector('.collapse');
+var mainMenu = document.getElementById('menu');
+var collapsible = document.querySelector('.collapsible');
+var menuLinks = document.querySelectorAll('.close-menu'); // Submit button hover
+
+submitButton.addEventListener('mouseover', function () {
+  heading.classList.add('pause');
 });
-sb.addEventListener("mouseout", function () {
-  lch.classList.remove('pause');
-});
+submitButton.addEventListener('mouseout', function () {
+  heading.classList.remove('pause');
+}); //Scroll to top when H4C logo is clicked
+
+var _iterator = _createForOfIteratorHelper(links),
+    _step;
+
+try {
+  for (_iterator.s(); !(_step = _iterator.n()).done;) {
+    var link = _step.value;
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      gsap.to(window, {
+        scrollTo: 0
+      });
+    });
+  } //Toggle (collapse/expand) Sub-menu
+
+} catch (err) {
+  _iterator.e(err);
+} finally {
+  _iterator.f();
+}
+
+collapsible.addEventListener('click', function (e) {
+  e.preventDefault();
+  subMenu.classList.toggle("active");
+}); //Show Main Menu
+
+var _iterator2 = _createForOfIteratorHelper(burgers),
+    _step2;
+
+try {
+  for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
+    var burger = _step2.value;
+    burger.addEventListener('click', function (e) {
+      e.preventDefault();
+      mainMenu.classList.remove('hidden');
+      app.classList.add('blur');
+    });
+  } //Hide Main Menu
+
+} catch (err) {
+  _iterator2.e(err);
+} finally {
+  _iterator2.f();
+}
+
+var _iterator3 = _createForOfIteratorHelper(menuLinks),
+    _step3;
+
+try {
+  for (_iterator3.s(); !(_step3 = _iterator3.n()).done;) {
+    var menuLink = _step3.value;
+    menuLink.addEventListener('click', handleMenuLinkClick);
+  }
+} catch (err) {
+  _iterator3.e(err);
+} finally {
+  _iterator3.f();
+}
+
+function handleMenuLinkClick(e) {
+  var href = this.getAttribute('href') || "";
+
+  if (href.indexOf("https") == -1) {
+    e.preventDefault();
+
+    if (this.classList.contains('icon-close-menu')) {
+      closeMenu();
+    } else {
+      var sectionID = href.substring(1);
+      var cnt = 0;
+      var scrollOffset = 0;
+
+      var _iterator4 = _createForOfIteratorHelper(cards),
+          _step4;
+
+      try {
+        for (_iterator4.s(); !(_step4 = _iterator4.n()).done;) {
+          var card = _step4.value;
+
+          if (card.getAttribute('id') == sectionID) {
+            scrollOffset = innerHeight + innerHeight * cnt / 2;
+            break;
+          }
+
+          cnt++;
+        }
+      } catch (err) {
+        _iterator4.e(err);
+      } finally {
+        _iterator4.f();
+      }
+
+      gsap.to(window, {
+        scrollTo: scrollOffset,
+        duration: 0,
+        onComplete: function onComplete() {
+          setTimeout(closeMenu, 200);
+        }
+      });
+    }
+  } else {
+    closeMenu();
+  }
+}
+
+function closeMenu() {
+  mainMenu.classList.add('hidden');
+  subMenu.classList.remove("active");
+  app.classList.remove('blur');
+}
+
+/***/ }),
+
+/***/ "./node_modules/gsap/dist/ScrollToPlugin.js":
+/*!**************************************************!*\
+  !*** ./node_modules/gsap/dist/ScrollToPlugin.js ***!
+  \**************************************************/
+/***/ (function(__unused_webpack_module, exports) {
+
+(function (global, factory) {
+	 true ? factory(exports) :
+	0;
+}(this, (function (exports) { 'use strict';
+
+	/*!
+	 * ScrollToPlugin 3.6.1
+	 * https://greensock.com
+	 *
+	 * @license Copyright 2008-2021, GreenSock. All rights reserved.
+	 * Subject to the terms at https://greensock.com/standard-license or for
+	 * Club GreenSock members, the agreement issued with that membership.
+	 * @author: Jack Doyle, jack@greensock.com
+	*/
+	var gsap,
+	    _coreInitted,
+	    _window,
+	    _docEl,
+	    _body,
+	    _toArray,
+	    _config,
+	    _windowExists = function _windowExists() {
+	  return typeof window !== "undefined";
+	},
+	    _getGSAP = function _getGSAP() {
+	  return gsap || _windowExists() && (gsap = window.gsap) && gsap.registerPlugin && gsap;
+	},
+	    _isString = function _isString(value) {
+	  return typeof value === "string";
+	},
+	    _isFunction = function _isFunction(value) {
+	  return typeof value === "function";
+	},
+	    _max = function _max(element, axis) {
+	  var dim = axis === "x" ? "Width" : "Height",
+	      scroll = "scroll" + dim,
+	      client = "client" + dim;
+	  return element === _window || element === _docEl || element === _body ? Math.max(_docEl[scroll], _body[scroll]) - (_window["inner" + dim] || _docEl[client] || _body[client]) : element[scroll] - element["offset" + dim];
+	},
+	    _buildGetter = function _buildGetter(e, axis) {
+	  var p = "scroll" + (axis === "x" ? "Left" : "Top");
+
+	  if (e === _window) {
+	    if (e.pageXOffset != null) {
+	      p = "page" + axis.toUpperCase() + "Offset";
+	    } else {
+	      e = _docEl[p] != null ? _docEl : _body;
+	    }
+	  }
+
+	  return function () {
+	    return e[p];
+	  };
+	},
+	    _clean = function _clean(value, index, target, targets) {
+	  _isFunction(value) && (value = value(index, target, targets));
+
+	  if (typeof value !== "object") {
+	    return _isString(value) && value !== "max" && value.charAt(1) !== "=" ? {
+	      x: value,
+	      y: value
+	    } : {
+	      y: value
+	    };
+	  } else if (value.nodeType) {
+	    return {
+	      y: value,
+	      x: value
+	    };
+	  } else {
+	    var result = {},
+	        p;
+
+	    for (p in value) {
+	      result[p] = p !== "onAutoKill" && _isFunction(value[p]) ? value[p](index, target, targets) : value[p];
+	    }
+
+	    return result;
+	  }
+	},
+	    _getOffset = function _getOffset(element, container) {
+	  element = _toArray(element)[0];
+
+	  if (!element || !element.getBoundingClientRect) {
+	    return console.warn("scrollTo target doesn't exist. Using 0") || {
+	      x: 0,
+	      y: 0
+	    };
+	  }
+
+	  var rect = element.getBoundingClientRect(),
+	      isRoot = !container || container === _window || container === _body,
+	      cRect = isRoot ? {
+	    top: _docEl.clientTop - (_window.pageYOffset || _docEl.scrollTop || _body.scrollTop || 0),
+	    left: _docEl.clientLeft - (_window.pageXOffset || _docEl.scrollLeft || _body.scrollLeft || 0)
+	  } : container.getBoundingClientRect(),
+	      offsets = {
+	    x: rect.left - cRect.left,
+	    y: rect.top - cRect.top
+	  };
+
+	  if (!isRoot && container) {
+	    offsets.x += _buildGetter(container, "x")();
+	    offsets.y += _buildGetter(container, "y")();
+	  }
+
+	  return offsets;
+	},
+	    _parseVal = function _parseVal(value, target, axis, currentVal, offset) {
+	  return !isNaN(value) && typeof value !== "object" ? parseFloat(value) - offset : _isString(value) && value.charAt(1) === "=" ? parseFloat(value.substr(2)) * (value.charAt(0) === "-" ? -1 : 1) + currentVal - offset : value === "max" ? _max(target, axis) - offset : Math.min(_max(target, axis), _getOffset(value, target)[axis] - offset);
+	},
+	    _initCore = function _initCore() {
+	  gsap = _getGSAP();
+
+	  if (_windowExists() && gsap && document.body) {
+	    _window = window;
+	    _body = document.body;
+	    _docEl = document.documentElement;
+	    _toArray = gsap.utils.toArray;
+	    gsap.config({
+	      autoKillThreshold: 7
+	    });
+	    _config = gsap.config();
+	    _coreInitted = 1;
+	  }
+	};
+
+	var ScrollToPlugin = {
+	  version: "3.6.1",
+	  name: "scrollTo",
+	  rawVars: 1,
+	  register: function register(core) {
+	    gsap = core;
+
+	    _initCore();
+	  },
+	  init: function init(target, value, tween, index, targets) {
+	    _coreInitted || _initCore();
+	    var data = this;
+	    data.isWin = target === _window;
+	    data.target = target;
+	    data.tween = tween;
+	    value = _clean(value, index, target, targets);
+	    data.vars = value;
+	    data.autoKill = !!value.autoKill;
+	    data.getX = _buildGetter(target, "x");
+	    data.getY = _buildGetter(target, "y");
+	    data.x = data.xPrev = data.getX();
+	    data.y = data.yPrev = data.getY();
+
+	    if (value.x != null) {
+	      data.add(data, "x", data.x, _parseVal(value.x, target, "x", data.x, value.offsetX || 0), index, targets);
+
+	      data._props.push("scrollTo_x");
+	    } else {
+	      data.skipX = 1;
+	    }
+
+	    if (value.y != null) {
+	      data.add(data, "y", data.y, _parseVal(value.y, target, "y", data.y, value.offsetY || 0), index, targets);
+
+	      data._props.push("scrollTo_y");
+	    } else {
+	      data.skipY = 1;
+	    }
+	  },
+	  render: function render(ratio, data) {
+	    var pt = data._pt,
+	        target = data.target,
+	        tween = data.tween,
+	        autoKill = data.autoKill,
+	        xPrev = data.xPrev,
+	        yPrev = data.yPrev,
+	        isWin = data.isWin,
+	        x,
+	        y,
+	        yDif,
+	        xDif,
+	        threshold;
+
+	    while (pt) {
+	      pt.r(ratio, pt.d);
+	      pt = pt._next;
+	    }
+
+	    x = isWin || !data.skipX ? data.getX() : xPrev;
+	    y = isWin || !data.skipY ? data.getY() : yPrev;
+	    yDif = y - yPrev;
+	    xDif = x - xPrev;
+	    threshold = _config.autoKillThreshold;
+
+	    if (data.x < 0) {
+	      data.x = 0;
+	    }
+
+	    if (data.y < 0) {
+	      data.y = 0;
+	    }
+
+	    if (autoKill) {
+	      if (!data.skipX && (xDif > threshold || xDif < -threshold) && x < _max(target, "x")) {
+	        data.skipX = 1;
+	      }
+
+	      if (!data.skipY && (yDif > threshold || yDif < -threshold) && y < _max(target, "y")) {
+	        data.skipY = 1;
+	      }
+
+	      if (data.skipX && data.skipY) {
+	        tween.kill();
+	        data.vars.onAutoKill && data.vars.onAutoKill.apply(tween, data.vars.onAutoKillParams || []);
+	      }
+	    }
+
+	    if (isWin) {
+	      _window.scrollTo(!data.skipX ? data.x : x, !data.skipY ? data.y : y);
+	    } else {
+	      data.skipY || (target.scrollTop = data.y);
+	      data.skipX || (target.scrollLeft = data.x);
+	    }
+
+	    data.xPrev = data.x;
+	    data.yPrev = data.y;
+	  },
+	  kill: function kill(property) {
+	    var both = property === "scrollTo";
+
+	    if (both || property === "scrollTo_x") {
+	      this.skipX = 1;
+	    }
+
+	    if (both || property === "scrollTo_y") {
+	      this.skipY = 1;
+	    }
+	  }
+	};
+	ScrollToPlugin.max = _max;
+	ScrollToPlugin.getOffset = _getOffset;
+	ScrollToPlugin.buildGetter = _buildGetter;
+	_getGSAP() && gsap.registerPlugin(ScrollToPlugin);
+
+	exports.ScrollToPlugin = ScrollToPlugin;
+	exports.default = ScrollToPlugin;
+
+	Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
+
 
 /***/ }),
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,14 @@
-var gsap = require('gsap/dist/gsap').gsap;
-var ScrollTrigger = require('gsap/dist/ScrollTrigger').ScrollTrigger;
+const gsap = require('gsap/dist/gsap').gsap;
+const ScrollTrigger = require('gsap/dist/ScrollTrigger').ScrollTrigger;
+const ScrollToPlugin = require('gsap/dist/ScrollToPlugin').ScrollToPlugin;
 
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollToPlugin);
 
-var app = document.getElementById("app");
+const app = document.getElementById("app");
+
 //Register a new timeline object
-var tl = gsap.timeline(
-{
+const tl = gsap.timeline({
 	// Attach it to the scroll
 	scrollTrigger: {
 		trigger: app,
@@ -17,53 +19,152 @@ var tl = gsap.timeline(
 		invalidateOnRefresh: true,
   		onRefresh: self => tl.progress(self.progress)
 	}
-}
-);
+});
 
-var d = 1; //animation duration
-var cards = document.querySelectorAll('.card');
+const d = 1; //animation duration
+const cards = document.querySelectorAll('.card');
 
 /**
- * Timeline animation
+ * Timeline animations
  */
 
 //Move the first two cards into the center position
-tl.fromTo('.card:nth-child(2)', 			{y:"100%"}, 	{y:0, 	duration: d, ease: 'linear'});
-tl.fromTo('.card:nth-child(2) .container', 	{y:"-100%"}, 	{y:0, 	duration: d, ease: 'linear'}, 	"-="+d);
-tl.fromTo('.card:nth-child(3)', 			{y:"-100%"},	{y:0, 	duration: d, ease: 'linear'}, 	"-="+d);
-tl.fromTo('.card:nth-child(3) .container',  {y:"100%"},		{y:0, 	duration: d, ease: 'linear'},	"-="+d);
+tl.fromTo('.card:nth-child(2)', 				{y:"100%"}, 	{y:0, 	duration: d, ease: 'linear'});
+tl.fromTo('.card:nth-child(2) .container', 		{y:"-100%"}, 	{y:0, 	duration: d, ease: 'linear'}, 	"-="+d);
+tl.fromTo('.card:nth-child(2) .nav-element', {y:"-100vh"}, 	{y:0, 	duration: d, ease: 'linear'}, 	"-="+d);
+
+tl.fromTo('.card:nth-child(3)', 				{y:"-100%"},	{y:0, 	duration: d, ease: 'linear'}, 	"-="+d);
+tl.fromTo('.card:nth-child(3) .container',  	{y:"100%"},		{y:0, 	duration: d, ease: 'linear'},	"-="+d);
+tl.fromTo('.card:nth-child(3) .nav-element', {y:"100vh"}, 	{y:0, 	duration: d, ease: 'linear'}, 	"-="+d);
 
 //Hide the hero
-tl.to('.hero:nth-child(1)', {y:"100%", 	duration: 0, ease: 'linear'});
+tl.to('#hero', {y:"100%", 	duration: 0, ease: 'linear'});
 
 //Animate all the cards
-for(var i = 1; i < cards.length-2; i+=2) {
-	          //target                                  //from       //to           //duration      //delay
-	tl.fromTo('.card:nth-child('+(i+1)+')', 			{y:0}, 		 {y:"-100%", 	duration: d, ease: 'linear'});
-	tl.fromTo('.card:nth-child('+(i+1)+') .container', 	{y:0}, 		 {y:"100%", 	duration: d, ease: 'linear'}, 	"-="+d);
-	tl.fromTo('.card:nth-child('+(i+3)+')', 			{y:"100%"},  {y:0, 			duration: d, ease: 'linear'},	"-="+d);
-	tl.fromTo('.card:nth-child('+(i+3)+') .container', 	{y:"-100%"}, {y:0, 			duration: d, ease: 'linear'}, 	"-="+d);
-	tl.fromTo('.card:nth-child('+(i+2)+')', 			{y:0},		 {y:"100%", 	duration: d, ease: 'linear'}, 	"-="+d);
-	tl.fromTo('.card:nth-child('+(i+2)+') .container',  {y:0},		 {y:"-100%", 	duration: d, ease: 'linear'},	"-="+d);
-	tl.fromTo('.card:nth-child('+(i+4)+')', 			{y:"-100%"}, {y:0, 			duration: d, ease: 'linear'}, 	"-="+d);
-	tl.fromTo('.card:nth-child('+(i+4)+') .container',  {y:"100%"},  {y:0, 			duration: d, ease: 'linear'}, 	"-="+d);
+for(let i = 1; i < cards.length-2; i+=2) {
+	          //target                                  	//from       //to           //duration      //delay
+	tl.fromTo('.card:nth-child('+(i+1)+')', 				{y:0}, 		 {y:"-100%", 	duration: d, ease: 'linear'});
+	tl.fromTo('.card:nth-child('+(i+1)+') .container', 		{y:0}, 		 {y:"100%", 	duration: d, ease: 'linear'}, 	"-="+d);
+	tl.fromTo('.card:nth-child('+(i+1)+') .nav-element', {y:0}, 		 {y:"100vh", 	duration: d, ease: 'linear'}, 	"-="+d);
+
+	tl.fromTo('.card:nth-child('+(i+3)+')', 				{y:"100%"},  {y:0, 			duration: d, ease: 'linear'},	"-="+d);
+	tl.fromTo('.card:nth-child('+(i+3)+') .container', 		{y:"-100%"}, {y:0, 			duration: d, ease: 'linear'}, 	"-="+d);
+	tl.fromTo('.card:nth-child('+(i+3)+') .nav-element', {y:"-100vh"},{y:0, 			duration: d, ease: 'linear'}, 	"-="+d);
+	
+	tl.fromTo('.card:nth-child('+(i+2)+')', 				{y:0},		 {y:"100%", 	duration: d, ease: 'linear'}, 	"-="+d);
+	tl.fromTo('.card:nth-child('+(i+2)+') .container',  	{y:0},		 {y:"-100%", 	duration: d, ease: 'linear'},	"-="+d);
+	tl.fromTo('.card:nth-child('+(i+2)+') .nav-element', {y:0}, 		 {y:"-100vh", 	duration: d, ease: 'linear'}, 	"-="+d);
+
+	tl.fromTo('.card:nth-child('+(i+4)+')', 				{y:"-100%"}, {y:0, 			duration: d, ease: 'linear'}, 	"-="+d);
+	tl.fromTo('.card:nth-child('+(i+4)+') .container',  	{y:"100%"},  {y:0, 			duration: d, ease: 'linear'}, 	"-="+d);
+	tl.fromTo('.card:nth-child('+(i+4)+') .nav-element', {y:"100vh"}, {y:0, 			duration: d, ease: 'linear'}, 	"-="+d);
 }
 
 //Move the last two cards off stage to reveal the final section
-tl.fromTo('.card:nth-child('+(cards.length)+')', 			{y:0}, 	{y:"-100%", 	duration: d, ease: 'linear'});
-tl.fromTo('.card:nth-child('+(cards.length)+') .container', 	{y:0}, 	{y:"100%", 	duration: d, ease: 'linear'}, 	"-="+d);
-tl.fromTo('.card:nth-child('+(cards.length+1)+')', 			{y:0},	{y:"100%", 	duration: d, ease: 'linear'}, 	"-="+d);
-tl.fromTo('.card:nth-child('+(cards.length+1)+') .container',  {y:0},		{y:"-100%", 	duration: d, ease: 'linear'},	"-="+d);
+tl.fromTo('.card:nth-child('+(cards.length)+')', 				{y:0}, 	{y:"-100%", 	duration: d, ease: 'linear'});
+tl.fromTo('.card:nth-child('+(cards.length)+') .container', 	{y:0}, 	{y:"100%", 		duration: d, ease: 'linear'}, 	"-="+d);
+tl.fromTo('.card:nth-child('+(cards.length)+') .nav-element',{y:0},  {y:"100vh", 	duration: d, ease: 'linear'}, 	"-="+d);
+
+tl.fromTo('.card:nth-child('+(cards.length+1)+')', 					{y:0}, {y:"100%", 	duration: d, ease: 'linear'}, 	"-="+d);
+tl.fromTo('.card:nth-child('+(cards.length+1)+') .container',  		{y:0}, {y:"-100%", 	duration: d, ease: 'linear'},	"-="+d);
+tl.fromTo('.card:nth-child('+(cards.length+1)+') .nav-element',  {y:0}, {y:"-100vh", duration: d, ease: 'linear'}, 	"-="+d);
 
 
-//Submit button hover event
-var lch = document.querySelector('.hero:last-child .heading'); 
-var sb = document.querySelector('.hero:last-child .submit');
-sb.addEventListener ("mouseover", function() {
-	lch.classList.add('pause');
+
+/**
+ * Event handlers
+ */
+
+const heading = document.querySelector('#the-end .heading'); 
+const submitButton = document.querySelector('#the-end .submit');
+const links = document.querySelectorAll('.link-home');
+const burgers = document.querySelectorAll(('.hamburger-menu'));
+const subMenu = document.querySelector(('.collapse'));
+const mainMenu = document.getElementById('menu');
+const collapsible = document.querySelector(('.collapsible'));
+const menuLinks = document.querySelectorAll(('.close-menu'));
+
+
+// Submit button hover
+submitButton.addEventListener ('mouseover', function() {
+	heading.classList.add('pause');
 });
-sb.addEventListener ("mouseout", function() {
-	lch.classList.remove('pause');
+submitButton.addEventListener ('mouseout', function() {
+	heading.classList.remove('pause');
 });
+
+
+//Scroll to top when H4C logo is clicked
+for (const link of links) {
+	link.addEventListener('click', function(e) {
+		e.preventDefault();
+		gsap.to(window, {scrollTo: 0});
+	});
+}
+
+
+//Toggle (collapse/expand) Sub-menu
+collapsible.addEventListener('click', function(e) {
+	e.preventDefault();
+	subMenu.classList.toggle("active");
+});
+
+//Show Main Menu
+for (const burger of burgers) {
+	burger.addEventListener('click', function(e) {
+		e.preventDefault();
+		mainMenu.classList.remove('hidden');
+		app.classList.add('blur');
+	});
+}
+
+//Hide Main Menu
+for (const menuLink of menuLinks) {
+	menuLink.addEventListener('click', handleMenuLinkClick);
+}
+
+function handleMenuLinkClick(e) {
+	
+	const href = this.getAttribute('href') || "";
+
+	if(href.indexOf("https") == -1) {
+		e.preventDefault();
+		
+		if(this.classList.contains('icon-close-menu')) {
+			closeMenu();
+		}
+		else {
+			let sectionID = href.substring(1);
+			let cnt = 0;
+			let scrollOffset = 0;
+
+			for(const card of cards) {
+				if (card.getAttribute('id') == sectionID) {
+					scrollOffset = innerHeight + (innerHeight * cnt/2);
+					break;
+				}
+				cnt++;
+			}
+
+			gsap.to(window, {
+				scrollTo: scrollOffset,
+				duration: 0,
+				onComplete: function() {
+					setTimeout(closeMenu, 200);
+				}
+			});
+		}
+	}
+	else {
+		closeMenu();
+	}
+}
+
+function closeMenu() {
+	mainMenu.classList.add('hidden');
+	subMenu.classList.remove("active");
+	app.classList.remove('blur');
+}
+
 
 
