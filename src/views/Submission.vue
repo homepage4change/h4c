b2071d19cdf22601f5629cc8451de6afb358c379
Pramesh Attwala <cheers@prameshattwala.com>
2021-09-20 18:32:12 -0400

    Fix desktop nav on Submission page

--- src/views/Submission.vue
+++ src/views/Submission.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="app" class="page page-submission">
-    <!-- <a href="/" class="link-home">
-      <inline-svg :src="require('../images/h4c-logo.svg')"></inline-svg>
-    </a> -->
+    <headerTop v-if="isDesktop"></headerTop>
     <iframe id="typeform-full" width="100%" height="100%" frameborder="0" allow="autoplay; encrypted-media;" src="https://form.typeform.com/to/BRPH7fLk?typeform-medium=embed-snippet"></iframe>
   </div>
 </template>
 
 <script>
+import headerTop from '../components/header-top.vue'
+
 export default {
   name: 'Submission',
+
+  components: {
+    headerTop
+  },
+
+  data () {
+    return {
+      isDesktop: true
+    }
+  },
 
   metaInfo () {
     const pageTitle = 'Application Submission'
@@ -36,6 +46,11 @@ export default {
     const typeformScript = document.createElement('script')
     typeformScript.setAttribute('src', '//embed.typeform.com/next/embed.js')
     document.head.appendChild(typeformScript)
+
+    this.isDesktop = (window.innerWidth > 767)
+    window.addEventListener('resize', () => {
+      this.isDesktop = (window.innerWidth > 767)
+    })
   }
 }
 </script>
